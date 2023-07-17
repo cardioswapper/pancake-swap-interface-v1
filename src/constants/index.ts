0599c2206f8ac016445f25b707316a525ceb8c72
cardioswapper <admin@cardioswap.io>
2023-07-17 13:38:16 +0200

    cardio router address

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,6 @@
 import { ChainId, JSBI, Percent, Token, WETH } from '@pancakeswap-libs/sdk'
 
-export const ROUTER_ADDRESS = '0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F'
+export const ROUTER_ADDRESS = '0x54E372D7192ceD71d9Eca3aB0bdB2A71B08F8FBb'
 
 // a list of tokens by chain
 type ChainTokenList = {
